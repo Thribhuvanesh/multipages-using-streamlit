c17f05c6644f2d28b90a9e31196caf5ffa5ca470
Thribhuvanesh <157610604+Thribhuvanesh@users.noreply.github.com>
2024-02-18 20:38:45 +0530

    Add files via upload

---
+++ multipage/pages/2-my projects.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("My Projects")
+st.write("1.Report using ML algorithms")
+st.write("2.Research paper on Data privacy WRT GDPR")
+st.write("3.Presented a paper on the Impacts of startups on the Indian economy.")
